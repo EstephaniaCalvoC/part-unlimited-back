f384ebd0b5f126e1f19c42fb7d1d1d68a96f06d1
Estephania Calvo <estephania.calvo.c@gmail.com>
2025-04-16 11:38:00 -0500

    Fix model

--- app/db/models/part.py
+++ app/db/models/part.py
@@ -6,12 +6,11 @@ from app.db.sqlalchemy_adapter import Base
 
 
 class Part(Base):
-    print("\n\n###### Part model start")
     __tablename__ = "part"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    sku = Column(String, nullable=False)
-    description = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(150), nullable=False)
+    sku = Column(String(30), nullable=False, unique=True)
+    description = Column(String(1024), nullable=False)
     weight_ounces = Column(Integer, nullable=False)
     is_active = Column(Boolean, nullable=False, default=True)
